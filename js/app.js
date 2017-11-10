d722d4604bf935abb679ca74f14f7357e7ebffef
betsy18 <betsy_dessire@hotmail.com>
2017-11-10 02:54:46 -0500

    Modificando el archivo .js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,53 @@
 // se ejecuta cuando el obj, este totalmente cargado la pag. web
 window.onload = function() {
-    var board = document.querySelector('.board-js');
-    //var reset = document.querySelector('.reset-js');
+  var board = document.querySelector('.board-js');
+  // var reset = document.querySelector('.reset-js');
 
-    board.addEventListener('click', addX);
-    //reset.removeEventListener('onclick', "td");
+  board.addEventListener('click', addX);
+  // reset.removeEventListener('onclick', "td");
 
 };
-    var centinel = true;
-
-    function addX(event){
-        if(event.target.matches('td') && event.target.textContent === '') {
-            if(centinel)
-                event.target.textContent = 'X';
-            else
-                event.target.textContent = 'O';
-            centinel = !centinel;
-        }
-    }
-
-    //var contenedor = document.getElementById('contenedor');
-    //var parrafo = document.getElementById('parrafo');
-    //contenedor.removeChild(parrafo);
     
-    /*
+var centinel = true;
+
+function addX(event) {
+  if (event.target.matches('td') && event.target.textContent === '') {
+    if (centinel)
+      event.target.textContent = 'X';
+    else
+      event.target.textContent = 'O';
+    centinel = !centinel;
+  }
+}
+
+function win() {
+  window.alert('Ganaste!');
+}
+
+function equal() {
+  window.alert('Empate..');
+}
+
+var td = document.querySelectorAll('td');
+var tdListA = [...td];
+
+function clear() {
+    
+}
+// var contenedor = document.getElementById('contenedor');
+// var parrafo = document.getElementById('parrafo');
+// contenedor.removeChild(parrafo);
+    
+/*
     Con el nodo seleccionado utilizamos parentNode
     para seleccionar el padre. Desde el padre utilizamos
     removeChild para eliminar el nodo seleccionado.
     */
     
-    //var reset = document.querySelector('.reset-js');
-    //parrafo.parentNode.removeChild(parrafo);
+// var reset = document.querySelector('.reset-js');
+// parrafo.parentNode.removeChild(parrafo);
 
-   /* function take(event){
+/* function take(event){
         if(event.target.textContent === 'X' || event.target.textContent === 'O'){
             var cells = event.target.getElementsByTagName('td');
             for( var i=0; i<td.length; i++){
@@ -41,29 +56,25 @@ window.onload = function() {
         }
     }*/
 
-
-
-    /* Creando el boton de reset*/
-
-    //reset.removeEventListener('click' , addX); 
-
-    /*function reset(event){
+/* Creando el boton de reset*/
+// reset.removeEventListener('click' , addX); 
+/* function reset(event){
         event.target.clear();
     }*/
 
-    /*function clear(){
+/* function clear(){
         event.target.matches('td');
       }*/
 
-      // utilizar el remove
+// utilizar el remove
 
-   /* var reset = document.querySelector('.reset-js');
+/* var reset = document.querySelector('.reset-js');
     reset.addEventListener('click', function(event){
         event.target.style.backgroundColor = 'white';
     });*/
 
 
-    /*function reset(event){
+/* function reset(event){
         var reset = document.getElementsByClassName('.reset-js');
 
         reset.addEventListener('click');
